92bc8571f1e426ee7962b5a14766e30ee546e721
Eskander <ehassan422@rku.ac.in>
2022-09-18 04:03:11 +0530

    commiting changes

--- file2.c
+++ file2.c
@@ -1,7 +1,10 @@
 #include <stdio.h>
 
-void main()
+
+int main()
 {
+	
 	printf("testing Jenkins");
+	return 0;
 
 }
--- file2.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-
-//using namespace std;
-int main()
-{
-	printf("testing Jenkins");
-	return 0;
-
-}
